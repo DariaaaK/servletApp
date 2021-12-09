cc75c3835e3658bd572e672e1a1881cb6d15bdb7
Daria <93216326+DariaaaK@users.noreply.github.com>
2021-12-09 17:03:49 +0200

    Added filters

--- src/main/java/com/example/demo/session/LoginRepository.java
+++ src/main/java/com/example/demo/session/LoginRepository.java
@@ -5,18 +5,18 @@ import java.util.Map;
 
 public class LoginRepository {
 
-     public static Map<String,String> mapUser = new HashMap<>();
+    public static Map<String,String> mapUser = new HashMap<>();
 
-     public static void initMapUser(){
-         mapUser.put("admin", "password");
-         mapUser.put("Daria", "Daria");
+    public static void initMapUser(){
+        mapUser.put("admin", "password");
+        mapUser.put("vlad", "vlad");
 
-     }
+    }
 
-     public static boolean chekMapUser( String login, String password) {
-         if (mapUser.containsKey(login) && mapUser.get(login).equals(password)) {
-             return true;
-         }
-         return false;
-     }
+    public static boolean chekMapUser( String login, String password) {
+        if (mapUser.containsKey(login) && mapUser.get(login).equals(password)) {
+            return true;
+        }
+        return false;
+    }
 }
